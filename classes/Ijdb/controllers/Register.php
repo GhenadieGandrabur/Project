7f2c7779f870bff6729b3094c7a548f2f0ac7750
Ghenadie Gandrabur <ghenadiigandrabur@gmail.com>
2022-04-30 21:22:13 +0300

    Registration

--- classes/Ijdb/controllers/Register.php
+++ classes/Ijdb/controllers/Register.php
@@ -33,34 +33,57 @@ class Register
     public function registerUser()
     {
         $author = $_POST['author'];
+
+        //Assume the data is valid to begin with
         $valid = true;
         $errors = [];
-        if (empty($author['name'])){
+
+        //But if any of the fields have been left blank, set $valid to false
+        if (empty($author['name'])) {
             $valid = false;
             $errors[] = 'Name cannot be blank';
         }
+
         if (empty($author['email'])) {
             $valid = false;
             $errors[] = 'Email cannot be blank';
         } else if (filter_var($author['email'], FILTER_VALIDATE_EMAIL) == false) {
             $valid = false;
             $errors[] = 'Invalid email address';
+        } else { //if the email is not blank and valid:
+            //convert the email to lowercase
+            $author['email'] = strtolower($author['email']);
+
+            //search for the lowercase version of `$author['email']`
+            if (count($this->authorsTable->find('email', $author['email'])) > 0) {
+                $valid = false;
+                $errors[] = 'That email address is already registered';
+            }
         }
+
+
         if (empty($author['password'])) {
             $valid = false;
             $errors[] = 'Password cannot be blank';
         }
 
-if ($valid == true)
-        {
+        //If $valid is still true, no fields were blank and the data can be added
+        if ($valid == true) {
+            $author['password'] = password_hash($author['password'],PASSWORD_DEFAULT);
+            //When submitted, the $author variable now contains a lowercase value for email
             $this->authorsTable->save($author);
 
-        header('Location: /author/success');
+            header('Location: /author/success');
+        } else {
+            //If the data is not valid, show the form again
+            return [
+                'template' => 'register.html.php',
+                'title' => 'Register an account',
+                'variables' => [
+                    'errors' => $errors,
+                    'author' => $author
+                ]
+            ];
+        }
     }
-    else{
-return ['template' => 'register.html.php', 'title' => 'Registre an account',
- 'variables' => ['errors' => $errors, 'author' => $author]];
-    }
-
-    }    
 }
--- classes/Ninja/DatabaseTable.php
+++ classes/Ninja/DatabaseTable.php
@@ -1,29 +1,36 @@
 <?php
+
 namespace Ninja;
-class DatabaseTable {
+
+class DatabaseTable
+{
 	private $pdo;
 	private $table;
 	private $primaryKey;
 
-	public function __construct(\PDO $pdo, string $table, string $primaryKey) {
+	public function __construct(\PDO $pdo, string $table, string $primaryKey)
+	{
 		$this->pdo = $pdo;
 		$this->table = $table;
 		$this->primaryKey = $primaryKey;
 	}
 
-	private function query($sql, $parameters = []) {
+	private function query($sql, $parameters = [])
+	{
 		$query = $this->pdo->prepare($sql);
 		$query->execute($parameters);
 		return $query;
-	}	
+	}
 
-	public function total() {
+	public function total()
+	{
 		$query = $this->query('SELECT COUNT(*) FROM `' . $this->table . '`');
 		$row = $query->fetch();
 		return $row[0];
 	}
 
-	public function findById($value) {
+	public function findById($value)
+	{
 		$query = 'SELECT * FROM `' . $this->table . '` WHERE `' . $this->primaryKey . '` = :value';
 
 		$parameters = [
@@ -35,8 +42,21 @@ class DatabaseTable {
 		return $query->fetch();
 	}
 
+	public function find($column, $value)
+	{
+		$query = 'SELECT * FROM ' . $this->table . ' WHERE ' . $column . ' = :value';
 
-	private function insert($fields) {
+		$parameters = [
+			'value' => $value
+		];
+
+		$query = $this->query($query, $parameters);
+
+		return $query->fetchAll();
+	}
+
+	private function insert($fields)
+	{
 		$query = 'INSERT INTO `' . $this->table . '` (';
 
 		foreach ($fields as $key => $value) {
@@ -62,8 +82,9 @@ class DatabaseTable {
 	}
 
 
-	private function update($fields) {
-		$query = ' UPDATE `' . $this->table .'` SET ';
+	private function update($fields)
+	{
+		$query = ' UPDATE `' . $this->table . '` SET ';
 
 		foreach ($fields as $key => $value) {
 			$query .= '`' . $key . '` = :' . $key . ',';
@@ -82,20 +103,23 @@ class DatabaseTable {
 	}
 
 
-	public function delete($id ) {
+	public function delete($id)
+	{
 		$parameters = [':id' => $id];
 
 		$this->query('DELETE FROM `' . $this->table . '` WHERE `' . $this->primaryKey . '` = :id', $parameters);
 	}
 
 
-	public function findAll() {
+	public function findAll()
+	{
 		$result = $this->query('SELECT * FROM ' . $this->table);
 
 		return $result->fetchAll();
 	}
 
-	private function processDates($fields) {
+	private function processDates($fields)
+	{
 		foreach ($fields as $key => $value) {
 			if ($value instanceof \DateTime) {
 				$fields[$key] = $value->format('Y-m-d');
@@ -106,15 +130,15 @@ class DatabaseTable {
 	}
 
 
-	public function save($record) {
+	public function save($record)
+	{
 		try {
 			if ($record[$this->primaryKey] == '') {
 				$record[$this->primaryKey] = null;
 			}
 			$this->insert($record);
-		}
-		catch (\PDOException $e) {
-			$this->update( $record);
+		} catch (\PDOException $e) {
+			$this->update($record);
 		}
 	}
 }
