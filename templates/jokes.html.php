d21c356e90a822c36ad389dcd44c106da8238990
Ghenadie Gandrabur <ghenadiigandrabur@gmail.com>
2022-08-08 17:29:14 +0300

    repear error

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,4 +1,4 @@
-<p><?= $totalJokes ?> jokes have been submitted to the Internet Joke Database.</p>
+<p><?= $variables['totalJokes'] ?> jokes have been submitted to the Internet Joke Database.</p>
 
 <?php foreach ($jokes as $joke) : ?>
   <blockquote>
