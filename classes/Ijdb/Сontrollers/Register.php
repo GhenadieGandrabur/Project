b166968d8a7757dec3737dc8c8553f19b563d908
GandraburGhenadii <ghenadii@kenwood.md>
2022-08-16 22:32:08 +0300

    git undo

--- classes/Ijdb/Сontrollers/Register.php
+++ classes/Ijdb/Сontrollers/Register.php
@@ -1,6 +1,14 @@
 <?php
-class RegisterController{
 
+namespace Ijdb\Controllers;
+
+use \Ninja\DatabaseTable;
+class RegisterController{
+    private $authorsTable;
+    public function __construct(DatabaseTable $authorsTable)
+    {
+        $this->authorsTable = $authorsTable;
+    }
     public function showForm(){
 
     }
